3df4b7c0871bb7c0473749512bd0c1a8a2e1e967
KillerKei <keiron.bates@gmail.com>
2020-12-30 05:31:44 +0000

    :urp-keys: Hotwire etc Added

---
+++ resources/[urp]/urp-keys/hotwire_s.lua
@@ -0,0 +1,111 @@
+local vehicleKeys = {}
+local myVehicleKeys = {}
+
+local robbableItems = {
+  [1] = {chance = 3, id = 0, quantity = math.random(25, 75)}, -- really common
+  [2] = {chance = 4, id = '2578778090', quantity = math.random(1, 1)}, -- rare
+  [3] = {chance = 3, id = 'mobilephone', quantity = math.random(1, 1)}, -- really common
+  [4] = {chance = 7, id = 'rubber', quantity = math.random(1,2)}, -- rare
+  [5] = {chance = 8, id = 'rolexwatch', quantity = 1},
+  [6] = {chance = 8, id = 'highgradefemaleseed', quantity = math.random(1, 3)},
+  [7] = {chance = 6, id = 'highgrademaleseed', quantity = math.random(1, 4)},
+  [8] = {chance = 4, id = 'rubber', quantity = math.random(1, 5)},
+  [9] = {chance = 5, id = 'oxy', quantity = math.random(1,2)},
+
+
+}
+
+RegisterServerEvent('garage:searchItem')
+AddEventHandler('garage:searchItem', function(plate)
+ local source = tonumber(source)
+ local item = {}
+ local ident = GetPlayerIdentifiers(source)[1]
+
+  item = robbableItems[math.random(1, #robbableItems)]
+  if math.random(1, 9) >= item.chance then
+   if tonumber(item.id) == 0 then
+    TriggerClientEvent('cash:add', source, item.quantity)
+    TriggerClientEvent("DoLongHudText", source, 'You found $'..item.quantity)
+   elseif tonumber(item.id) == 1 then
+    TriggerClientEvent("DoLongHudText", source, 'You have found the keys to the vehicle!')
+    vehicleKeys[plate] = {}
+    table.insert(vehicleKeys[plate], {id = ident})
+    TriggerClientEvent('garage:updateKeys', source, vehicleKeys, ident)
+    TriggerClientEvent('vehicle:start', source)
+   else
+    TriggerClientEvent('player:receiveItem', source, item.id, item.quantity)
+    TriggerClientEvent("DoLongHudText", source, 'Item Added!')
+   end
+  else
+    TriggerClientEvent("DoLongHudText", source, 'You found nothing', 2)
+  end
+end)
+
+
+--[[URPCore.RegisterServerCallback('disc-hotwire:checkOwner', function(source, cb, plate)
+  local player = URPCore.GetPlayerFromId(source)
+  MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE owner = @identifier AND plate = @plate', {
+      ['@identifier'] = player.identifier,
+      ['@plate'] = plate
+  }, function(results)
+      cb(#results == 1)
+  end)
+end)]]--
+
+RegisterServerEvent('garage:giveKey')
+AddEventHandler('garage:giveKey', function(target, plate)
+ local targetSource = tonumber(target)
+ local ident = GetPlayerIdentifiers(targetSource)[1]
+ local ident2 = GetPlayerIdentifiers(source)[1]
+ local plate = tostring(plate)
+
+ vehicleKeys[plate] = {}
+ table.insert(vehicleKeys[plate], {id = ident})
+ --TriggerClientEvent('chatMessage', targetSource, 'You just recieved keys to a vehicle')
+ TriggerClientEvent("DoLongHudText", source, 'You just recieved keys to a vehicle')
+ TriggerClientEvent('garage:updateKeys', targetSource, vehicleKeys, ident)
+ --re-enable to only have one set of keys
+ --TriggerClientEvent('garage:updateKeys', source, vehicleKeys, ident2)
+end)
+
+RegisterServerEvent('garage:addKeys')
+AddEventHandler('garage:addKeys', function(plate)
+ local source = tonumber(source)
+ local ident = GetPlayerIdentifiers(source)[1]
+ while plate == nil do
+  Citizen.Wait(5)
+ end
+
+ if vehicleKeys[plate] ~= nil then
+  table.insert(vehicleKeys[plate], {id = ident})
+  TriggerClientEvent('garage:updateKeys', source, vehicleKeys, ident)
+ else
+  vehicleKeys[plate] = {}
+  table.insert(vehicleKeys[plate], {id = ident})
+  TriggerClientEvent('garage:updateKeys', source, vehicleKeys, ident)
+ end
+end)
+
+
+RegisterServerEvent('garage:removeKeys')
+AddEventHandler('garage:removeKeys', function(plate)
+ local source = tonumber(source)
+ local ident = GetPlayerIdentifiers(source)[1]
+ if vehicleKeys[plate] ~= nil then
+  for id,v in pairs(vehicleKeys[plate]) do
+   if v.id == ident then
+    table.remove(vehicleKeys[plate], id)
+   end
+  end
+ end
+ TriggerClientEvent('garage:updateKeys', source, vehicleKeys, ident)
+end)
+
+RegisterServerEvent('removelockpick')
+AddEventHandler('removelockpick', function()
+ local source = tonumber(source)
+ if math.random(1, 20) == 1 then
+  TriggerClientEvent('player:receiveItem', source, "lockpick", 1)
+  TriggerClientEvent("DoLongHudText", source, 'The lockpick bent out of shape.')
+ end
+end)
