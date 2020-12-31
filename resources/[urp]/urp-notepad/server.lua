fc4cb0faed71a83a2572bae47a1b2aed3ee108b1
KillerKei <keiron.bates@gmail.com>
2020-12-31 01:15:45 +0000

    :urp-notepad:

---
+++ resources/[urp]/urp-notepad/server.lua
@@ -0,0 +1,25 @@
+local serverNotes = {}
+
+RegisterServerEvent("server:destroyNote")
+AddEventHandler("server:destroyNote", function(closestNoteId)
+    table.remove(serverNotes,closestNoteId)
+    TriggerClientEvent('client:updateNotesRemove', -1, closestNoteId)
+end)
+
+RegisterServerEvent("server:newNote")
+AddEventHandler("server:newNote", function(text,x,y,z)
+    serverNotes[#serverNotes+1] = { text = text, x = x, y = y, z = z }
+    TriggerClientEvent('client:updateNotesAdd', -1, { text = text, x = x, y = y, z = z })
+    Citizen.Wait(1)
+    TriggerClientEvent('client:updateNotes', src, serverNotes)
+end)
+
+RegisterServerEvent("server:requestNotes")
+AddEventHandler("server:requestNotes", function()
+    local src = source
+    TriggerClientEvent('client:updateNotes', src, serverNotes)
+end)
+
+RegisterCommand('notepad', function(source)
+    TriggerClientEvent('Notepad:open', source)
+end)
