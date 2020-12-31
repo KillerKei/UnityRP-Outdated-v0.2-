b7b2808e0b36db8fa701a68ae181bb0f7b032914
Sterious <68664092+Sterious7@users.noreply.github.com>
2020-12-31 02:18:56 +0000

    :urp-drugdeliveries: Update

--- resources/[urp]/urp-drugdeliveries/client.lua
+++ resources/[urp]/urp-drugdeliveries/client.lua
@@ -99,9 +99,9 @@ local drugLocs = {
 	[3] =  { ['x'] = -676.81,['y'] = -877.94,['z'] = 24.48,['h'] = 324.9, ['info'] = ' Wei Cheng' },
 }
 
-local pillStore =  { ['x'] = 590.26,['y'] = 2745.01,['z'] = 10.22,['h'] = 72.58, ['info'] = ' oxy' }
+local pillStore =  { ['x'] = 591.26,['y'] = 2744.11,['z'] = 42.05,['h'] = 5.53, ['info'] = ' oxy' }
 
-local pillWorker = { ['x'] = 591.26,['y'] = 2744.11,['z'] = 42.05,['h'] = 5.53, ['info'] = ' lol' }
+local pillWorker = { ['x'] = 590.89,['y'] = 2747.82,['z'] = 15.86,['h'] = 200.65, ['info'] = ' lol' }
 
 
 function buildDrugShop()
@@ -111,56 +111,57 @@ function buildDrugShop()
 	Citizen.Wait(1000)
 
 	local generator = { x = pillStore["x"] , y = pillStore["y"], z = pillStore["z"] - 35.0}
-  	SetEntityCoords(PlayerPedId(),generator.x,generator.y,generator.z)
+  	SetEntityCoords(PlayerPedId(),generator.x,generator.y,generator.z+2)
   	
-	local building = CreateObject(`V_38_C_Barbers_Shell`,generator.x-0.31811000,generator.y+1.79183500,generator.z+1.56171400,false,false,false)
+	local building = CreateObject(`traphouse_shell`,generator.x-0.31811000,generator.y+1.79183500,generator.z+2.56171400,false,false,false)
+	FreezeEntityPosition(building, true)
+	local coordsofbuilding = GetEntityCoords(building, true)
+	-- CreateObject(`V_38_C_Barbers_Det`,coordsofbuilding.x+0.36036100,coordsofbuilding.y-0.35528500,coordsofbuilding.z+1.54137200,false,false,false)
+	-- CreateObject(`V_38_C_CABINET02`,coordsofbuilding.x+0.37338400,coordsofbuilding.y-3.67517500,coordsofbuilding.z+1.48056600,false,false,false)
 
-	CreateObject(`V_38_C_Barbers_Det`,generator.x+0.36036100,generator.y-0.35528500,generator.z+1.54137200,false,false,false)
-	CreateObject(`V_38_C_CABINET02`,generator.x+0.37338400,generator.y-3.67517500,generator.z+1.48056600,false,false,false)
-
-	CreateObject(`V_38_FAN`,generator.x+0.18863910,generator.y-1.78123200,generator.z+1.49853700,false,false,false)
-	CreateObject(`V_38_SHELVES`,generator.x+0.38722500,generator.y-0.21649000,generator.z+2.34309200,false,false,false)
-	CreateObject(`V_38_Pictures`,generator.x+0.36021210,generator.y+0.34026300,generator.z+1.82029300,false,false,false)
-	CreateObject(`V_38_LIGHTS`,generator.x+0.95295510,generator.y-0.34358800,generator.z+4.50438800,false,false,false)
+	-- CreateObject(`V_38_FAN`,coordsofbuilding.x+0.18863910,coordsofbuilding.y-1.78123200,coordsofbuilding.z+1.49853700,false,false,false)
+	-- CreateObject(`V_38_SHELVES`,coordsofbuilding.x+0.38722500,coordsofbuilding.y-0.21649000,coordsofbuilding.z+2.34309200,false,false,false)
+	-- CreateObject(`V_38_Pictures`,coordsofbuilding.x+0.36021210,coordsofbuilding.y+0.34026300,coordsofbuilding.z+1.82029300,false,false,false)
+	-- CreateObject(`V_38_LIGHTS`,coordsofbuilding.x+0.95295510,coordsofbuilding.y-0.34358800,coordsofbuilding.z+4.50438800,false,false,false)
 	
-	local plant = CreateObject(`V_38_BARB_PLANT02`,generator.x+3.01122600,generator.y-4.98704700,generator.z+2.91572800,false,false,false)
-	SetEntityRotation(plant,0.0,0.0,170.0,2,1)
+	-- local plant = CreateObject(`V_38_BARB_PLANT02`,coordsofbuilding.x+3.01122600,coordsofbuilding.y-4.98704700,coordsofbuilding.z+2.91572800,false,false,false)
+	-- SetEntityRotation(plant,0.0,0.0,170.0,2,1)
 
-	local chair1 = CreateObject(`Prop_chair_01b`,generator.x+2.92626000,generator.y+0.70815100,generator.z+1.54303900,false,false,false)
-	local chair2 = CreateObject(`prop_chair_04a`,generator.x+2.92626000,generator.y+2.21829500,generator.z+1.54303900,false,false,false)
-	local chair3 = CreateObject(`prop_chair_02`,generator.x+2.92626000,generator.y+1.44866100,generator.z+1.54303900,false,false,false)
-	local chair4 = CreateObject(`prop_chair_02`,generator.x+2.92626000,generator.y+0.00554299,generator.z+1.54303900,false,false,false)
+	-- local chair1 = CreateObject(`Prop_chair_01b`,coordsofbuilding.x+2.92626000,coordsofbuilding.y+0.70815100,coordsofbuilding.z+1.54303900,false,false,false)
+	-- local chair2 = CreateObject(`prop_chair_04a`,coordsofbuilding.x+2.92626000,coordsofbuilding.y+2.21829500,coordsofbuilding.z+1.54303900,false,false,false)
+	-- local chair3 = CreateObject(`prop_chair_02`,coordsofbuilding.x+2.92626000,coordsofbuilding.y+1.44866100,coordsofbuilding.z+1.54303900,false,false,false)
+	-- local chair4 = CreateObject(`prop_chair_02`,coordsofbuilding.x+2.92626000,coordsofbuilding.y+0.00554299,coordsofbuilding.z+1.54303900,false,false,false)
 
-	SetEntityRotation(chair1,0.0,0.0,-90.0,2,1)
-	SetEntityRotation(chair2,0.0,0.0,-80.0,2,1)
-	SetEntityRotation(chair3,0.0,0.0,-85.0,2,1)
-	SetEntityRotation(chair4,0.0,0.0,-95.0,2,1)
+	-- SetEntityRotation(chair1,0.0,0.0,-90.0,2,1)
+	-- SetEntityRotation(chair2,0.0,0.0,-80.0,2,1)
+	-- SetEntityRotation(chair3,0.0,0.0,-85.0,2,1)
+	-- SetEntityRotation(chair4,0.0,0.0,-95.0,2,1)
 
-	CreateObject(`prop_tv_05`,generator.x+2.97058500,generator.y+4.72485000,generator.z+3.96126500,false,false,false)
-	local tv = CreateObject(`prop_tv_05`,generator.x-2.20064500,generator.y-4.56200100,generator.z+3.96016800,false,false,false)
-	SetEntityRotation(tv,0.0,0.0,190.0,2,1)
+	-- CreateObject(`prop_tv_05`,coordsofbuilding.x+2.97058500,coordsofbuilding.y+4.72485000,coordsofbuilding.z+3.96126500,false,false,false)
+	-- local tv = CreateObject(`prop_tv_05`,coordsofbuilding.x-2.20064500,coordsofbuilding.y-4.56200100,coordsofbuilding.z+3.96016800,false,false,false)
+	-- SetEntityRotation(tv,0.0,0.0,190.0,2,1)
 	
-	local til = CreateObject(`prop_till_01`,generator.x-2.18592300,generator.y-1.87080100,generator.z+2.51398500,false,false,false)
-	SetEntityRotation(til,0.0,0.0,180.0,2,1)
-	CreateObject(`v_ret_gc_fan`,generator.x+2.94997000,generator.y+3.27074200,generator.z+1.49715400,false,false,false)
+	-- local til = CreateObject(`prop_till_01`,coordsofbuilding.x-2.18592300,coordsofbuilding.y-1.87080100,coordsofbuilding.z+2.51398500,false,false,false)
+	-- SetEntityRotation(til,0.0,0.0,180.0,2,1)
+	-- CreateObject(`v_ret_gc_fan`,coordsofbuilding.x+2.94997000,coordsofbuilding.y+3.27074200,coordsofbuilding.z+1.49715400,false,false,false)
 	
-	CreateObject(`prop_cctv_cam_06a`,generator.x-2.35117100,generator.y+4.86646700,generator.z+4.18179800,false,false,false)
-	CreateObject(`prop_game_clock_01`,generator.x-0.54486800,generator.y+5.01194300,generator.z+3.67846000,false,false,false)
-	CreateObject(`prop_radio_01`,generator.x+3.07343200,generator.y+3.16888200,generator.z+3.37168900,false,false,false)
+	-- CreateObject(`prop_cctv_cam_06a`,coordsofbuilding.x-2.35117100,coordsofbuilding.y+4.86646700,coordsofbuilding.z+4.18179800,false,false,false)
+	-- CreateObject(`prop_game_clock_01`,coordsofbuilding.x-0.54486800,coordsofbuilding.y+5.01194300,coordsofbuilding.z+3.67846000,false,false,false)
+	-- CreateObject(`prop_radio_01`,coordsofbuilding.x+3.07343200,coordsofbuilding.y+3.16888200,coordsofbuilding.z+3.37168900,false,false,false)
 	
-	CreateObject(`prop_speaker_05`,generator.x-2.40189600,generator.y+0.54597100,generator.z+3.89755000,false,false,false)
-	CreateObject(`prop_speaker_05`,generator.x-2.40189600,generator.y+3.19824400,generator.z+3.16581200,false,false,false)
-	CreateObject(`prop_rub_stool`,generator.x-2.20233000,generator.y+4.06275700,generator.z+1.52316500,false,false,false)
-	CreateObject(`prop_watercooler`,generator.x-2.26554100,generator.y+2.82748200,generator.z+1.41562700,false,false,false)
-	CreateObject(`V_38_C_SHADOWMAP`,generator.x-0.31811000,generator.y+1.79183500,generator.z+1.54171400,false,false,false)
+	-- CreateObject(`prop_speaker_05`,coordsofbuilding.x-2.40189600,coordsofbuilding.y+0.54597100,coordsofbuilding.z+3.89755000,false,false,false)
+	-- CreateObject(`prop_speaker_05`,coordsofbuilding.x-2.40189600,coordsofbuilding.y+3.19824400,coordsofbuilding.z+3.16581200,false,false,false)
+	-- CreateObject(`prop_rub_stool`,coordsofbuilding.x-2.20233000,coordsofbuilding.y+4.06275700,coordsofbuilding.z+1.52316500,false,false,false)
+	-- CreateObject(`prop_watercooler`,coordsofbuilding.x-2.26554100,coordsofbuilding.y+2.82748200,coordsofbuilding.z+1.41562700,false,false,false)
+	-- CreateObject(`V_38_C_SHADOWMAP`,coordsofbuilding.x-0.31811000,coordsofbuilding.y+1.79183500,coordsofbuilding.z+1.54171400,false,false,false)
 
 	
-	CreateObject(`V_38_BARB_PLANT003`,generator.x-2.30056400,generator.y+1.66849900,generator.z+2.38898200,false,false,false)
-	CreateObject(`V_38_C_Pictures3`,generator.x+3.20509200,generator.y-0.40208200,generator.z+1.91242400,false,false,false)
-	CreateObject(`V_38_C_Sink`,generator.x-0.62845000,generator.y+4.84067900,generator.z+1.41538000,false,false,false)
+	-- CreateObject(`V_38_BARB_PLANT003`,coordsofbuilding.x-2.30056400,coordsofbuilding.y+1.66849900,coordsofbuilding.z+2.38898200,false,false,false)
+	-- CreateObject(`V_38_C_Pictures3`,coordsofbuilding.x+3.20509200,coordsofbuilding.y-0.40208200,coordsofbuilding.z+1.91242400,false,false,false)
+	-- CreateObject(`V_38_C_Sink`,coordsofbuilding.x-0.62845000,coordsofbuilding.y+4.84067900,coordsofbuilding.z+1.41538000,false,false,false)
 
-	FreezeEntityPosition(building,true)
-	SetEntityCoords(PlayerPedId(), 592.26, 2745.01, 10.22)
+	-- FreezeEntityPosition(coordsofbuilding,true)
+	SetEntityCoords(PlayerPedId(), 590.41, 2740.0, 15.8)
 	Citizen.Wait(500)
 	SetEntityHeading(PlayerPedId(),0.0)
 	FreezeEntityPosition(PlayerPedId(),false)
@@ -1210,8 +1211,8 @@ Citizen.CreateThread(function()
 
 	    local dropOff2 = #(vector3(GetEntityCoords(PlayerPedId())) - vector3(chopinfo[2]["x"],chopinfo[2]["y"],chopinfo[2]["z"]))
 	  --  local dropOff3 = #(vector3(GetEntityCoords(PlayerPedId())) - vector3(chopinfo[3]["x"],chopinfo[3]["y"],chopinfo[3]["z"]))
-	    local dropOff4 = #(GetEntityCoords(PlayerPedId()) - vector3(pillStore["x"]-1.4,pillStore["y"]-4.47,pillStore["z"]-32.2))
-	    local dropOff5 = #(GetEntityCoords(PlayerPedId()) - vector3(pillStore["x"],pillStore["y"],pillStore["z"]))
+	    local dropOff4 = #(GetEntityCoords(PlayerPedId()) - vector3(590.4242, 2739.596,15.85879))
+		local dropOff5 = #(GetEntityCoords(PlayerPedId()) - vector3(-1271.262, -1148.615, 6.791))
 	    local dropOff6 = #(GetEntityCoords(PlayerPedId()) - vector3(pillWorker["x"],pillWorker["y"],pillWorker["z"]))
 
 	    local GoldBars = #(GetEntityCoords(PlayerPedId()) - vector3(-112.58, 6468.93, 31.63))
@@ -1242,15 +1243,15 @@ Citizen.CreateThread(function()
 		end
 
 		if dropOff4 < 1.5 then
-			DrawText3Ds(pillStore["x"]-1.4,pillStore["y"]-4.47,pillStore["z"]-32.2, "[E] to Leave") 
+			DrawText3Ds(590.4242, 2739.596,15.85879, "[E] to Leave")
 			if IsControlJustReleased(0,38) then
 				CleanUpArea()
-				SetEntityCoords(PlayerPedId(),pillStore["x"],pillStore["y"],pillStore["z"])
+				SetEntityCoords(PlayerPedId(),-1271.262, -1148.615, 6.791)
 				Citizen.Wait(1000)
 			end
 		end
 		if dropOff5 < 1.5 then
-			DrawText3Ds(pillStore["x"],pillStore["y"],pillStore["z"], "[E] to Enter") 
+			DrawText3Ds(-1271.262, -1148.615, 6.791, "[E] to Enter") 
 			if IsControlJustReleased(0,38) then
 				buildDrugShop()
 				
