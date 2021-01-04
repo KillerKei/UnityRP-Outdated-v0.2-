66acedd69b319d0f0ea28fd43011c1f12590660a
KillerKei <keiron.bates@gmail.com>
2021-01-04 22:31:13 +0000

    :3dme: Updated

--- resources/[Assets]/3dme/fxmanifest.lua
+++ resources/[Assets]/3dme/fxmanifest.lua
@@ -2,6 +2,5 @@ fx_version 'adamant'
 games { 'gta5' }
 
 client_script 'client.lua'
-client_script "@irp-errorlog/client/cl_errorlog.lua"
-server_script '@mysql-async/lib/MySQL.lua'
+client_script "@urp-errorlog/client/cl_errorlog.lua"
 server_script 'server.lua'
