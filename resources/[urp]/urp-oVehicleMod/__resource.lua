476ba148249b07bb017274466b1e34179cf6e043
KillerKei <keiron.bates@gmail.com>
2021-01-04 13:24:44 +0000

    :urp-oVehicleMod: Fixed error

--- resources/[urp]/urp-oVehicleMod/__resource.lua
+++ resources/[urp]/urp-oVehicleMod/__resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
-client_script "@irp-errorlog/client/cl_errorlog.lua"
+client_script "@urp-errorlog/client/cl_errorlog.lua"
 
 client_script 'client.lua'
 server_script 'server.lua'
