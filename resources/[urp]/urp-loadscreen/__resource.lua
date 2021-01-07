023e092ed5194fe20762b99b476fb3a8fab735a0
PixelRez <lukes123456@hotmail.com>
2021-01-07 20:22:50 +0000

    new loadscreen POGGERS ASF

--- resources/[urp]/urp-loadscreen/__resource.lua
+++ resources/[urp]/urp-loadscreen/__resource.lua
@@ -1,11 +1,13 @@
 files {
     'index.html',
-    'style.css',
-    'logo.png',
-    'background.jpg',
-    
+    'css/style.css',
+    'png.png',
+    'gif.gif',
+    'Discord.png',
+    'audio.mp3',
+    'video.js'
 }
 
 loadscreen 'index.html'
-client_script "@urp-errorlog/client/cl_errorlog.lua"
+
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
