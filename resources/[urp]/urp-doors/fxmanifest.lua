053994ea64907ad633722aef2301bb0125c1bb71
PixelRez <lukes123456@hotmail.com>
2021-01-09 18:28:29 +0000

    Real doorlock :)

---
+++ resources/[urp]/urp-doors/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+games { 'rdr3', 'gta5' }
+
+author 'whitewingz'
+description 'One City Doors'
+version '1.0.0'
+
+-- dependency "np-base"
+dependency "ghmattimysql"
+
+shared_script "shared/sh_doors.lua"
+
+server_script "server/sv_doors.lua"
+client_script "client/cl_doors.lua"
+client_script "@urp-errorlog/client/cl_errorlog.lua"
+
+server_export 'isDoorLocked'
