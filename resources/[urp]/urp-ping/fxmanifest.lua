7d11c8951afa3c498674634b4ed0eccce3c838c7
KillerKei <keiron.bates@gmail.com>
2021-01-11 22:39:16 +0000

    :urp-ping: Push

---
+++ resources/[urp]/urp-ping/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+client_scripts {
+    'config.lua',
+	'client/main.lua',
+	"@urp-errorlog/client/cl_errorlog.lua"
+}
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'server/main.lua',
+}
